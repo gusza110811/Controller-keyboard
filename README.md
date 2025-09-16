eb381d3a2b9cc395305621523470756518ec26bb
gusza <100755292+gusza110811@users.noreply.github.com>
2025-09-16 21:41:38 +0700

    readme and license

---
+++ README.md
@@ -0,0 +1,4 @@
+# Controller Keyboard
+A definitely very good way to use a controller to type
+
+<footer>Copyright &copy; S*** 'Gusza' N*** 2025</footer>
--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import tkinter as tk
+from tkinter import scrolledtext
 import threading
 import time
+import os
+import sys
 from controller import Controller
+
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+class GnuNotice:
+    def __init__(self):
+        self.agree = os.path.exists(".agree")
+        if self.agree:
+            return
+        with open("agree","w") as agree:
+            agree.write("If you choose to stop agreeing to the terms and conditions, you are free to uninstall the software")
+        with open("LICENSE") as license:
+            self.file = license.read()
+        self.root = tk.Tk()
+        self.root.title("Terms and Conditions")
+        self.text = scrolledtext.ScrolledText(self.root)
+        self.text.insert(tk.END,self.file)
+        self.text.config(state="disabled")
+        self.text.pack()
+
+        self.label = tk.Label(self.root,text="By continuing to use this software,\nyou agree to the license above", font=("Helvetica","15"))
+        self.label.pack()
+
+        self.main()
+
+    def main(self):
+        self.root.mainloop()
 
 class PreviewWindow:
     def __init__(self, controller: Controller):
@@ -33,7 +66,12 @@ class PreviewWindow:
 
 
 if __name__ == "__main__":
+    import makelicense
+    makelicense.makeLicense()
     controller = Controller()
     preview = PreviewWindow(controller)
+    
+    notice = threading.Thread(target=lambda:GnuNotice(), daemon=False)
+    notice.start()
 
     preview.run()
