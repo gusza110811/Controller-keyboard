225851e27b9fad1c6b95b099a32ac7ed00f3aa95
gusza <100755292+gusza110811@users.noreply.github.com>
2025-09-16 20:06:15 +0700

    yeah

---
+++ controller.py
@@ -0,0 +1,107 @@
+import inputs
+import pyautogui
+import threading
+
+class Controller:
+    def __init__(self):
+        listener = threading.Thread(target=self.listen,daemon=True)
+        #   pr, up, dn, lt, rt
+        # N
+        # U
+        # R
+        # D
+        # L
+        # P
+        self.MAPPING = [
+            'e', 'o', 'a', 'i', 'u',
+            'l', 't', 'h', 'r', 'd',
+            'y', 'n', 's', 'm', 'w',
+            'v', 'f', 'g', 'p', 'b',
+            'c', 'k', 'j', 'x', 'z',
+            ' ', '.', ',', ':', 'q',
+        ]
+        self.keys = {
+            "U":False,
+            "L":False,
+            "R":False,
+            "D":False,
+            "P":False,
+
+            "LT":False,
+            "RT":False,
+        }
+        listener.start()
+
+    def listen(self):
+        ignore = 0
+        while True:
+            events:list[inputs.InputEvent] = inputs.get_gamepad()
+            for event in events:
+                if event.code == "BTN_THUMBL":
+                    if event.state == 1:
+                        self.keys["P"] = True
+                    else:
+                        self.keys["P"] = False
+                
+                if event.code == "BTN_WEST" and event.state == 1:
+                    pyautogui.hotkey('backspace')
+
+                if event.code == "BTN_THUMBR" and event.state == 1:
+                    self.write(0)
+                elif event.state > 2**14:
+                    if event.code == "ABS_RY":
+                        if ignore != 1:
+                            ignore = 1
+                            self.write(1)
+                    elif event.code == "ABS_RX":
+                        if ignore != 4:
+                            ignore = 4
+                            self.write(4)
+                    else:
+                        ignore = 0
+                    
+                    if event.code == "ABS_Y":
+                        self.keys["U"] = True
+                        self.keys["L"] = False
+                        self.keys["R"] = False
+                        self.keys["D"] = False
+                    elif event.code == "ABS_X":
+                        self.keys["U"] = False
+                        self.keys["L"] = False
+                        self.keys["R"] = True
+                        self.keys["D"] = False
+
+                elif event.state < -2**14:
+                    if event.code == "ABS_RY":
+                        if ignore != 2:
+                            ignore = 2
+                            self.write(2)
+                    elif event.code == "ABS_RX":
+                        if ignore != 3:
+                            ignore = 3
+                            self.write(3)
+                    else:
+                        ignore = 0
+                    if event.code == "ABS_Y":
+                        self.keys["U"] = False
+                        self.keys["L"] = False
+                        self.keys["R"] = False
+                        self.keys["D"] = True
+                    elif event.code == "ABS_X":
+                        self.keys["U"] = False
+                        self.keys["L"] = True
+                        self.keys["R"] = False
+                        self.keys["D"] = False
+
+                if (abs(event.state) < 2**14) and (event.code == "ABS_X" or event.code == "ABS_Y"):
+                    self.keys["U"] = False
+                    self.keys["L"] = False
+                    self.keys["R"] = False
+                    self.keys["D"] = False
+    
+    def write(self, id):
+        index = id + (5 if self.keys["U"] else 10 if self.keys["R"] else 15 if self.keys["D"] else 20 if self.keys["L"] else 25 if self.keys["P"] else 0)
+        pyautogui.write(self.MAPPING[index])
+
+if __name__ == "__main__":
+    controller = Controller()
